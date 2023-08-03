b37f8471a24f8bde1ee4e6a9f72c913bcc756340
Kargath <40135358+KargathEx@users.noreply.github.com>
2023-08-03 17:41:18 +0800

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,65 @@
+#------------------------------- Import Modules BEGIN -------------------------------
+# 引入 posh-git
+Import-Module posh-git
+
+# 引入 oh-my-posh
+# Import-Module oh-my-posh
+# 换成了如下
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\mingy\AppData\Local\Programs\oh-my-posh\themes\powerlevel10k_classic.omp.json' | Invoke-Expression
+# 引入 ps-read-line
+Import-Module PSReadLine
+
+# 设置 PowerShell 主题
+# Set-PoshPrompt ys
+# Set-PoshPrompt paradox
+#------------------------------- Import Modules END   -------------------------------
+
+#-------------------------------  Set Hot-keys BEGIN  -------------------------------
+# 设置预测文本来源为历史记录
+Set-PSReadLineOption -PredictionSource History
+
+# 每次回溯输入历史，光标定位于输入内容末尾
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+# 设置 Tab 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+
+# 设置 Ctrl+d 为退出 PowerShell
+Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
+
+# 设置 Ctrl+z 为撤销
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+
+# 设置向上键为后向搜索历史记录
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+
+# 设置向下键为前向搜索历史纪录
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+#-------------------------------  Set Hot-keys END    -------------------------------
+
+
+
+
+
+# 3. 查看目录 ls & ll
+function ListDirectory {
+	(Get-ChildItem).Name
+	Write-Host("")
+}
+Set-Alias -Name ls -Value ListDirectory
+Set-Alias -Name ll -Value Get-ChildItem
+
+# 4. 打开当前工作目录
+function OpenCurrentFolder {
+	param
+	(
+		# 输入要打开的路径
+		# 用法示例：open C:\
+		# 默认路径：当前工作文件夹
+		$Path = '.'
+	)
+	Invoke-Item $Path
+}
+Set-Alias -Name open -Value OpenCurrentFolder
+#-------------------------------    Set Alias END     -------------------------------
