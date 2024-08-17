9e0857fe3879f3bcea97cab2a926afd16ce621cb
mingy <40135358+mingyEx@users.noreply.github.com>
2024-08-17 18:58:42 +0800

    Create leetcodeThemeConvert.js

---
+++ leetcodeConvert.js
@@ -0,0 +1,3 @@
+javascript:if (!confirm(window.location.href.indexOf('classic') != -1 ? % 27更换默认主题 % 27 : % 27更换经典主题 % 27)) {}
+else { window.location.replace(window.location.href.indexOf(% 27classic % 27) != -1 ? window.location.href.replace(% 27classic / % 27, % 27 % 27) : window.location.href.replace(% 27problems / % 27, % 27classic / problems / % 27)); }
+//Saving the above as a bookmark allows you to switch between the classic theme and the default theme for those with small screens that don't make it easy to turn on drop-down menus!
