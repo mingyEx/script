94b2b23773fbb8448c00e7d5aa2228117af9bd84
mingy <40135358+mingyEx@users.noreply.github.com>
2024-12-06 00:12:23 +0800

    Update leetcodeConvert.js

--- leetcodeConvert.js
+++ leetcodeConvert.js
@@ -1,3 +1,7 @@
 javascript:if (!confirm(window.location.href.indexOf('classic') != -1 ? % 27更换默认主题 % 27 : % 27更换经典主题 % 27)) {}
 else { window.location.replace(window.location.href.indexOf(% 27classic % 27) != -1 ? window.location.href.replace(% 27classic / % 27, % 27 % 27) : window.location.href.replace(% 27problems / % 27, % 27classic / problems / % 27)); }
 //Saving the above as a bookmark allows you to switch between the classic theme and the default theme for those small-screen users who don't have easy access to drop-down menus!
+
+
+//don't have confirm version
+javascript:window.location.replace(window.location.href.indexOf(%27classic%27) != -1 ? window.location.href.replace(%27classic/%27,%27%27):window.location.href.replace(%27problems/%27,%27classic/problems/%27));
